0209b9ea94f816803b1d0e0470e774169eb678a6
Nytokon <dante.konoba@gmail.com>
2026-05-11 10:46:05 -0300

    Initial commit

---
+++ Assets/Scripts/Cronometro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cronometro : MonoBehaviour
+{
+public TextMeshProUGUI Txt_Tiempo;
+    public float tiempo = 0;
+
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        Txt_Tiempo.text = tiempo.ToString("F0");
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed=10;
+    public float aceleracion = 1;
+    public bool juegoIniciado = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public void Iniciador()
+    {
+        juegoIniciado = true;
+    }
+    void Update()
+   {
+        if (juegoIniciado)
+        {
+            speed += aceleracion * Time.deltaTime;
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+    }
+     }
+
